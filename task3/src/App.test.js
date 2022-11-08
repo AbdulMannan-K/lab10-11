15d4a2df6080093b41a360e488134e401d4cf9f6
Abdul Mannan <i200905@nu.edu.pk>
2022-11-08 15:51:02 +0500

    something

--- task3/src/App.test.js
+++ task3/src/App.test.js
@@ -1,9 +1,9 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByTestId("test");
-//   // expect(linkElement).toBeInTheDocument();
-//   expect(linkElement.textContent).toBe('Hello World');
-// });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId("test");
+  // expect(linkElement).toBeInTheDocument();
+  expect(linkElement.textContent).toBe('Hello World');
+});
